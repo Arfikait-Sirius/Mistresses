2d1352c8609a690a9adf4e16dea53130a0bad285
Arfikait Sirius <arfikait.sirius@gmail.com>
2021-09-12 00:19:46 +0900

    Learn Louise. - fn_lower_all() and fn_upper_first().

--- dir_Girls/Louise/louise.rb
+++ dir_Girls/Louise/louise.rb
@@ -13,4 +13,29 @@ class Louise
     def fn_upper_all( base )
         return base.upcase
     end
+
+    #------------------------
+    # :[ NAME ]:
+    #     fn_lower_all
+    #
+    # :[ CATEGORY ]:
+    #     Skill
+    #------------------------
+    def fn_lower_all( base )
+        return base.downcase
+    end
+
+    #------------------------
+    # :[ NAME ]:
+    #     fn_upper_first
+    #
+    # :[ CATEGORY ]:
+    #     Skill
+    #------------------------
+    def fn_upper_first( base )
+        upper = base.upcase
+        lower = base.downcase
+
+        return upper.slice( 0 ) + lower.slice( 1..base.length )
+    end
 end
