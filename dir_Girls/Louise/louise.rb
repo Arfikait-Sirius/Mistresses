25f3715a598b4a03e8c57940970368498d8fba36
Arfikait Sirius <arfikait.sirius@gmail.com>
2021-10-10 10:59:41 +0900

    Learn Louise. - fn_count(), fn_copy(), fn_replace(), fn_split(), fn_get_substring(), fn_trim(), fn_from_int() and fn_find().

--- dir_Girls/Louise/louise.rb
+++ dir_Girls/Louise/louise.rb
@@ -5,6 +5,50 @@ class Louise
 
     #------------------------
     # :[ NAME ]:
+    #     fn_count
+    #
+    # :[ CATEGORY ]:
+    #     Skill
+    #------------------------
+    def fn_count( base, target )
+        return base.scan( target ).length
+    end
+
+    #------------------------
+    # :[ NAME ]:
+    #     fn_copy
+    #
+    # :[ CATEGORY ]:
+    #     Skill
+    #------------------------
+    def fn_copy( base )
+        return base
+    end
+
+    #------------------------
+    # :[ NAME ]:
+    #     fn_replace
+    #
+    # :[ CATEGORY ]:
+    #     Skill
+    #------------------------
+    def fn_replace( base, target, replacement )
+        return base.sub( target, replacement )
+    end
+
+    #------------------------
+    # :[ NAME ]:
+    #     fn_split
+    #
+    # :[ CATEGORY ]:
+    #     Skill
+    #------------------------
+    def fn_split( base, splitter, index )
+        return base.split( splitter )[index]
+    end
+
+    #------------------------
+    # :[ NAME ]:
     #     fn_upper_all
     #
     # :[ CATEGORY ]:
@@ -37,5 +81,66 @@ class Louise
         lower = base.downcase
 
         return upper.slice( 0 ) + lower.slice( 1..base.length )
+    end
+
+    #------------------------
+    # :[ NAME ]:
+    #     fn_get_substring
+    #
+    # :[ CATEGORY ]:
+    #     Skill
+    #------------------------
+    def fn_get_substring( base, format_str )
+        if base.length < format_str.length
+            return nil
+        end
+
+        index = format_str.index( "%s" )
+        head = format_str.slice( 0..( index - 1 ) )
+        tail = format_str.slice( ( index + 2 )..format_str.length )
+
+        headIndex = base.index( head )
+        if headIndex == nil
+            return nil
+        end
+        tailIndex = base.index( tail )
+        if tailIndex == nil
+            return nil
+        end
+
+        return base.slice( ( headIndex + head.length )..tailIndex )
+    end
+
+    #------------------------
+    # :[ NAME ]:
+    #     fn_trim
+    #
+    # :[ CATEGORY ]:
+    #     Skill
+    #------------------------
+    def fn_trim( base )
+        return base.gsub( /^[ \t]*/, "" ).gsub( /[ \t]*$/, "" )
+    end
+
+    #------------------------
+    # :[ NAME ]:
+    #     fn_from_int
+    #
+    # :[ CATEGORY ]:
+    #     Skill
+    #------------------------
+    def fn_from_int( number )
+        return number.to_s
+    end
+
+    #------------------------
+    # :[ NAME ]:
+    #     fn_find
+    #
+    # :[ CATEGORY ]:
+    #     Skill
+    #------------------------
+    def fn_find( base, target )
+        return base.index( target ) + 1
     end
 end
