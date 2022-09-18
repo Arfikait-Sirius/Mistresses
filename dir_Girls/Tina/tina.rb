e1de94173cb292644a30b2c2a5f498ecb82dd8cf
Arfikait Sirius <arfikait.sirius@gmail.com>
2022-09-18 19:19:54 +0900

    Retake Tina. - Make up.

--- dir_Girls/Tina/tina.rb
+++ dir_Girls/Tina/tina.rb
@@ -8,9 +8,6 @@ class Tina
     LABEL_JUDGE = "[     JUDGE]: "
     LABEL_PRINT = "[     PRINT]: "
 
-    @@girl_name = ""
-    @@skill_name = ""
-
     #------------------------
     # :[ NAME ]:
     #     fn_set_girl_name
@@ -19,9 +16,8 @@ class Tina
     #     Skill
     #------------------------
     def self.fn_set_girl_name( name )
-        @@girl_name = name
 
-        puts( "#{LABEL_GIRLS_NAME}#{@@girl_name}" )
+        puts( "#{LABEL_GIRLS_NAME}#{name}" )
 
         return
     end
@@ -34,7 +30,6 @@ class Tina
     #     Skill
     #------------------------
     def self.fn_set_skill_name( target )
-        @@skill_name = target
 
         puts( "#{LABEL_TARGET}#{target}()" )
 
@@ -53,7 +48,7 @@ class Tina
         if judgement
             puts( "#{LABEL_JUDGE}OK" )
         else
-            puts( "#{LABEL_JUDGE}NG - Please check #{@@skill_name}" )
+            puts( "#{LABEL_JUDGE}NG - Please check this source." )
         end
 
         return
